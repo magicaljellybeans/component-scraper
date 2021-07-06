bed59b95890eb9e1cfa300a00989e185cdf970cf
Tom Robinson <thomas.robinson10@nhs.net>
2021-07-06 23:14:30 +0100

    rename variables/functions, modify to accept text file of URLs

--- component_scraper.py
+++ component_scraper.py
@@ -4,18 +4,18 @@ CLI Syntax: python component_scraper.py <csv file> <component class name>
 
 Script to compare <div> component heading sizes to previous "structural" 
 headings in the DOM.
-
 Outputs a csv file in the current working directory.
-Input CSV must have one column of URLs to scrape with the header "url".
+
+Input file is a text file with a URL path on each line.
 Output CSV has a column of URLs and titles of component headings that 
 triggered the condition.
 
-Previous "structural" headings are defined as children of a <section>
-or element with class "block-richtext" that occur sequentially before the 
-given component heading.
+Previous "structural" headings are defined as children of a <section> 
+tag or tag with class "block-richtext", that occur sequentially prior 
+to the given component heading tag.
 
-Current condition is a component heading being larger than the previous 
-"structural" heading.
+Current condition is a component heading being larger than the 
+previous "structural" heading.
 
 """
 import requests
@@ -29,28 +29,24 @@ from bs4 import BeautifulSoup
 heading_tags = re.compile("^h[1-6]$")
 
 
-def get_from_csv(file):
-    """Extract URLs to be scraped and add to a list."""
-    with open(file, mode="r") as csv_file:
-
+def read_urls_from_file(file):
+    with open(file, mode="r") as file:
         url_list = []
-        csv_reader = csv.DictReader(csv_file)
 
-        for row in csv_reader:
-            url = row["url"].replace("www.", r"https://")
-            url_list.append(url)
+        for line in file.readlines():
+            if "/nhsuk/" in line:
+                url = line.replace("/nhsuk", "https://www.nhs.uk").strip()
+                url_list.append(url)
 
     return url_list
 
 
 def look_for_issues(url_list, component_type):
-    """Add URLs and component headings that meet the condition to a dict."""
-    issue_dict = {}
-    len_list = len(url_list)
+    """Compend dict of faulty headers and urls they occur on"""
+    faulty_headers = {}
     bad_urls = []
 
     for index, url in enumerate(url_list):
-
         try:
             html = requests.get(url).text
         except requests.exceptions.RequestException:
@@ -58,71 +54,74 @@ def look_for_issues(url_list, component_type):
 
         soup = BeautifulSoup(html, "html.parser")
 
-        print(f"Scraping page {index + 1} of {len_list}")
+        print(f"Scraping page {index + 1} of {len(url_list)} ... {url}")
 
         components = soup.find_all("div", class_=f"{component_type}")
 
-        for component in components:
-            component_heading = component.find(heading_tags)
+        for target_component in components:
+            target_component_heading = target_component.find(heading_tags)
 
-            if component_heading is None:
+            if target_component_heading is None:
                 continue
 
-            component_heading_size = component_heading.name[1]
+            target_heading_size = target_component_heading.name[1]
 
-            previous_heading = find_previous_structural_heading(
-                component_heading.find_previous(heading_tags)
+            prior_heading = find_prior_structural_heading(
+                target_component_heading.find_previous(heading_tags)
             )
 
-            if previous_heading is None:
+            if prior_heading is None:
                 continue
 
-            previous_heading_size = previous_heading.name[1]
+            prior_heading_size = prior_heading.name[1]
 
-            if component_heading_size < previous_heading_size:
-                title = component_heading.text.strip()
+            if target_heading_size < prior_heading_size:
+                title = target_component_heading.text.strip()
 
                 print(f"    Incorrect sizing on {title}")
 
-                if url in issue_dict:
-                    issue_dict[url].append(title)
+                if url in faulty_headers:
+                    faulty_headers[url].append(title)
                 else:
-                    issue_dict[url] = [title]
+                    faulty_headers[url] = [title]
 
     for url in bad_urls:
         print(f"Bad URL: {url}")
 
-    print(f"Pages with issues: {len(issue_dict.keys())}")
+    print(f"Pages with issues: {len(faulty_headers.keys())}")
 
-    return issue_dict, component_type
+    return faulty_headers
 
 
-def find_previous_structural_heading(heading):
-    """Return the next previous heading when it meets a condition"""
+def find_prior_structural_heading(heading):
     if heading is None:
         return None
-    if heading.parent.name == "section" \
-            or "block-richtext" in heading.parent["class"]:
-        return heading
+    try:
+        if (
+            heading.parent.name == "section"
+            or "block-richtext" in heading.parent["class"]
+        ):
+            return heading
+    except KeyError:
+        # Some tags don't have the "class" attr and can throw KeyError
+        pass
 
-    find_previous_structural_heading(heading.find_previous(heading_tags))
+    find_prior_structural_heading(heading.find_previous(heading_tags))
 
 
-def write_to_csv(input):
-    """Write URLs and component headings to a CSV."""
-    issue_dict, component = input
-
+def write_to_csv(faulty_headers, component):
     with open(f"{component}_issue-list.csv", mode="w") as new_file:
         writer = csv.writer(new_file, delimiter=",")
 
-        for url in issue_dict:
-            writer.writerow([url, issue_dict[url]])
+        for url in faulty_headers:
+            for header in faulty_headers[url]:
+                writer.writerow([url, header])
 
 
 if __name__ == "__main__":
     file = sys.argv[1]
-    component_type = sys.argv[2]
+    component = sys.argv[2]
 
-    url_list = get_from_csv(file)
-    input = look_for_issues(url_list, component_type)
-    write_to_csv(input)
+    url_list = read_urls_from_file(file)
+    faulty_headers = look_for_issues(url_list, component)
+    write_to_csv(faulty_headers, component)
